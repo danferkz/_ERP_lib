6cfb41c6ae67bee7ef04924629959e8655966794
danferkz <gdanfer07@gmail.com>
2022-10-30 09:09:20 -0500

    nuevo

--- inicio/inicio.py
+++ inicio/inicio.py
@@ -1,10 +1,10 @@
 from tkinter import*
+import tkinter as tk
 
-class Alumno:
-    def __init__(self,ventana):
-        self.ventana=ventana
-        self.ventana.title("confirmacion")
-if __name__=="__main__":
-    ventana=Tk()
-    aplicacion=Alumno(ventana)
-    ventana.mainloop()
+ventana= tk.Tk()
+ventana.title("Login")
+ventana.geometry("1916x1364+500+50")
+ventana.resizable(width=False,height=False)
+fondo=tk.PhotoImage(file="imagenes/INICIO.png")
+fondo1 = tk.Label(ventana, image=fondo).place(x=0, y=0, relwidth=1, relheight=1)
+ventana.mainloop()
