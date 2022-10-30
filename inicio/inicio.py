3263469ad9de02f7a6c169bbeda99d83a8559b3d
danferkz <gdanfer07@gmail.com>
2022-10-30 11:07:04 -0500

    nuevo

--- inicio/inicio.py
+++ inicio/inicio.py
@@ -10,7 +10,7 @@ fondo_ENTRADA="#ffffff"
 
 
 ventana= tk.Tk()
-ventana.title("Login")
+ventana.title("INICIO")
 ventana.geometry("1916x1364+500+50")
 ventana.resizable(width=False,height=False)
 fondo=tk.PhotoImage(file="imagenes/nuevo fondo.png")
@@ -21,7 +21,8 @@ comtraseña=tk.StringVar()
 #entradas
 entrada=tk.Entry(ventana, text="USUARIO", width=32, bg=fondo_ENTRADA, font=("Inter", 50,), relief="flat")
 entrada.place(x=434, y=650)
-entrada2=tk.Entry(ventana, text="CONTRASEÑA", width=32, bg=fondo_ENTRADA, font=("Inter", 50,), relief="flat")
+entrada.insert(0,'USUARIO')
+entrada2=tk.Entry(ventana, text="CONTRASEÑA",fg="#7c7c7c", width=32, bg=fondo_ENTRADA, font=("Inter", 50), relief="flat")
 entrada2.place(x=434, y=850)
 #Botones
 boton1=tk.Button(ventana, text="CONECTAR", cursor="hand2", bg=fondo_CONECTAR, width=12, relief="flat", font=("Inter", 40,"bold"))
