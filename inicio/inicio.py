ea578a5fcd6ef8e27dd176da8ea0a0119ffcc6d7
danferkz <gdanfer07@gmail.com>
2022-11-01 14:48:15 -0500

    nuevo

--- inicio/inicio.py
+++ inicio/inicio.py
@@ -1,4 +1,4 @@
-from ast import literal_eval
+
 from tkinter import*
 import tkinter as tk
 
@@ -6,7 +6,10 @@ import tkinter as tk
 fondo_CONECTAR="#54556d"
 fondo_ENTRADA="#ffffff"
 fondo_fondo_check="#00b764"
-#entradas
+
+
+
+#VENTANA
 
 ventana= tk.Tk()
 ventana.title("INICIO")
@@ -25,7 +28,8 @@ def on_leave(a):
     if name=="":
         entrada.insert(0,'USUARIO')
         
-entrada=tk.Entry(ventana, text="USUARIO",fg="#7c7c7c", width=32, bg=fondo_ENTRADA, font=("Inter", 50,), relief="flat")
+#USUARIO
+entrada=tk.Entry(ventana,textvariable=usuario, text="USUARIO",fg="#7c7c7c", width=32, bg=fondo_ENTRADA, font=("Inter", 50,), relief="flat")
 entrada.place(x=434, y=650)
 entrada.insert(0,'USUARIO')
 entrada.bind('<FocusIn>', on_enter)
@@ -37,16 +41,38 @@ def on_leave(b):
     cont=entrada2.get()
     if cont=="":
         entrada2.insert(0,'CONTRASEÑA')
-
-entrada2=tk.Entry(ventana, text="CONTRASEÑA",fg="#7c7c7c", width=32, bg=fondo_ENTRADA, font=("Calibri ", 50), relief="flat")
+        
+#CONTRASEÑA
+entrada2=tk.Entry(ventana,textvariable=comtraseña, text="CONTRASEÑA",fg="#7c7c7c", width=32, bg=fondo_ENTRADA, font=("Calibri ", 50), relief="flat")
 entrada2.place(x=434, y=850)
 entrada2.insert(0,'CONTRASEÑA')
 entrada2.bind('<FocusIn>', on_enter)
 entrada2.bind('<FocusOut>', on_leave)
-#Botones
-boton1=tk.Button(ventana, text="CONECTAR",fg="#ffffff", cursor="hand2", bg=fondo_CONECTAR, width=12, relief="flat", font=("Inter", 40,"bold"))
-boton1.place(x=734, y=1185)
 
+
+
+def conectar():
+    usuario=entrada.get()
+    comtraseña=entrada2.get()
+    if usuario=="Daniel" and comtraseña=="1234":
+        print('hola')
+        correcta()
+    #else:
+        #incorrecta()
+        
+        
+def correcta():
+    ventana.withdraw()
+    window = tk.Toplevel()
+    window.title("Nuevo pestaña 2")
+    window.geometry('1917x1363+500+50')
+    window.resizable(width=False,height=False)
+    fondo=tk.PhotoImage(file="imagenes/SEGUNDA_ventana.png")
+    fondo2=tk.Label(window, image=fondo).place(x=0, y=0, relwidth=1, relheight=1)
+    ventana.mainloop()
+#Botones
+boton1=tk.Button(ventana,command=conectar, text="CONECTAR",fg="#ffffff", cursor="hand2", bg=fondo_CONECTAR, width=12, relief="flat", font=("Inter", 40,"bold"))
+boton1.place(x=734, y=1185)
 
 #def chekboton
 
